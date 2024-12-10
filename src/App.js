bcbb197d524a606017f02eac6d518985be653b1a
OSAMA-AHMED <121261957+OSAMA-2003@users.noreply.github.com>
2024-12-10 02:56:12 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+/** @format */
+import "./App.css";
+import axios from 'axios'
+import React from'react';
+
+class App extends React.Component {
+
+  state = {advice:''}
+
+  componentDidMount(){
+     this.fetchAdvice()
+  }
+
+  fetchAdvice = () =>{
+      axios.get('https://api.adviceslip.com/advice')
+      .then((response)=>{
+        const {advice}= response.data.slip   
+        this.setState({advice})
+      })
+      .catch((error)=>{
+          console.log(error)
+      })
+            
+  
+  }
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="btn" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+export default App;
